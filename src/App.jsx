489ff492e7d62d79089fdf30cc98b17d4daa4b46
utkarsh singh <132093287+utkarshsingh5499@users.noreply.github.com>
2026-06-03 13:45:58 +0530

    Change BrowserRouter to HashRouter in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { useState } from 'react'
 
 import Navbar from './components/Navbar'
